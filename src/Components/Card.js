f2ca18134cf63ac046950d844bf4ebd8d59e64d4
Chirag85 <42144372+Chirag85@users.noreply.github.com>
2019-03-10 16:14:13 +0530

    Update Card.js

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -8,7 +8,7 @@ class Card extends Component {
         <div className='he'>
          <h2 className='tc f'>{this.props.name}</h2>
          <br></br>
-         {t>0 ? <h3 className='f'>Temperature : {t.toFixed(2)} C</h3>: <h3 className='f'>Temperature : </h3>}
+         {this.props.temp ? <h3 className='f'>Temperature : {t.toFixed(2)} C</h3>: <h3 className='f'>Temperature : </h3>}
          <h3 className='f'>Pressure : {this.props.pressure}</h3>
          <h3 className='f'>Humidity : {this.props.humidity}</h3>
          <h3 className='f'>Description : {this.props.description}</h3>
